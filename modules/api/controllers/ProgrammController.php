e69660ecafaf1557e33c9c4ee7755e0ea4a321ec
Dmitriy Shkreba <d.shkreba@novotelecom.ru>
2021-02-08 03:00:59 +0700

    Контроллер программ

--- modules/api/controllers/ProgrammController.php
+++ modules/api/controllers/ProgrammController.php
@@ -5,18 +5,27 @@ namespace app\modules\api\controllers;
 
 
 use app\modules\api\models\ChannelProgramm;
+use app\modules\api\models\Channels;
 
 class ProgrammController extends \yii\rest\Controller
 {
 
     public function actionFind(){
         $channel_id = \Yii::$app->request->get('channel_id') ?? null;
-        $date = \Yii::$app->request->get('date_start') ?? '2021-02-07';
+        $date = \Yii::$app->request->get('date_start') ?? '2021-02-08';
 
+        $result = Channels::find()->asArray()->all();
         $query = ChannelProgramm::find();
         $query->date($date);
         if($channel_id)
             $query->channel($channel_id);
-        return $query->all();
+        $program = $query->all();
+        array_walk($result, function(&$channel) use($program){
+           $channel['items'] = array_values(array_filter($program, function($item) use ($channel){
+               return $item['channel_id'] == $channel['id'];
+           }));
+        });
+
+        return $result;
     }
 }
